a02ab595eff4132cbcec05fa31b4ef4f183317ac
Rashid <rashidmustafa3355@gmail.com>
2025-08-13 14:50:55 +0500

    conversion of data types

---
+++ datatypescoonversion.js
@@ -0,0 +1,26 @@
+let score = 80 // number to boolean
+
+console.log(typeof score)
+
+let valueInnumber = Boolean(score)
+
+console.log(typeof valueInnumber);
+console.log(valueInnumber);  // why this NaN
+
+let score1 = "33shxcv" // string to number
+
+console.log(typeof score1)
+
+let valueInnumber1 = Number(score1)
+
+console.log(typeof valueInnumber1);
+console.log(valueInnumber1);
+
+let score3 = true  // boolean to string
+
+console.log(typeof score3)
+
+let valueInnumber3 = String(score3)
+
+console.log(typeof valueInnumber3);
+console.log(valueInnumber3);

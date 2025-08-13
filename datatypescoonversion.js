2034696d836da97615770cb1789213f8fd551d2f
Rashid <rashidmustafa3355@gmail.com>
2025-08-13 15:01:34 +0500

    updates

--- datatypescoonversion.js
+++ datatypescoonversion.js
@@ -1,11 +1,21 @@
-let score = 80 // number to boolean
+let score = 0 // number to boolean
 
 console.log(typeof score)
 
 let valueInnumber = Boolean(score)
 
 console.log(typeof valueInnumber);
-console.log(valueInnumber);  // why this NaN
+console.log(valueInnumber); 
+//  number to string
+
+let num = 55
+
+console.log(typeof num)
+
+let valueInstring = String(num)
+
+console.log(typeof valueInstring);
+console.log(valueInstring);
 
 let score1 = "33shxcv" // string to number
 
@@ -16,7 +26,7 @@ let valueInnumber1 = Number(score1)
 console.log(typeof valueInnumber1);
 console.log(valueInnumber1);
 
-let score3 = true  // boolean to string
+let score3 = 1  // boolean to string
 
 console.log(typeof score3)
 

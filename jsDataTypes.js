4e051d392ea3c3901add5a042ce1391a9b0bfc4a
Rashid <rashidmustafa3355@gmail.com>
2025-08-13 13:41:37 +0500

    datatypes

---
+++ jsDataTypes.js
@@ -0,0 +1,19 @@
+
+
+// Data Types Pirmitive
+let name= "Rashid" // String => "" or ''
+let age= 28 // number 2^53
+let isloggedIn= true // boolean
+let state= null // null standalone value 
+let city; // undefined
+// symbol use for unqueness for 
+
+// non pirmitive data types
+// Object
+// function
+// array
+
+// we can find types of data types if we don't know
+
+console.log(typeof undefined);
+console.log(typeof null);
---
+++ jsPractice.js
@@ -0,0 +1,18 @@
+// first out put 
+// console.log(3+3)
+const firstName= "Muhmmad" 
+// const variable can be change it shows error
+// const firstName= "rashid" not allowed
+const secondNAme= " Rashid"
+// console.log(firstName+ secondNAme)
+let a=5;
+let b=6;
+// console.log(a+b)
+// let varibale values can be changed after declaration 
+a= 4;
+b= 10;
+let c= a+b;
+// console.log(c)
+// we can display mutiple variables in table form
+
+// console.table([firstName, secondNAme, c])

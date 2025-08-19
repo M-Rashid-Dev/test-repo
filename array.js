9783e70ceba5bf997afafd66a70be3964186c468
Rashid <rashidmustafa3355@gmail.com>
2025-08-20 02:22:56 +0500

    array

---
+++ array.js
@@ -0,0 +1,18 @@
+//  array
+const array =[6, 1, "rashid", 'a']
+console.log(array[3]);
+
+const arr2 = new Array(1,2,3,"rashid")
+// console.log(arr2[2,3]);
+
+// array method
+
+//  puch add value in array
+
+arr2.push(6)
+arr2.push(8)
+arr2.unshift(7)
+arr2.shift()
+console.log(arr2);
+console.log(arr2.includes("rashid"));
+console.log(arr2.indexOf("rashid"));

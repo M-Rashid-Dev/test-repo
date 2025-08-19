dc7b379c9d2911363a3de3efdebb04112aa7b682
Rashid <rashidmustafa3355@gmail.com>
2025-08-19 23:54:31 +0500

    date and time

---
+++ dateandtime.js
@@ -0,0 +1,30 @@
+let myDate = new Date()
+console.log(myDate);
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toTimeString());
+console.log(myDate.toUTCString());
+
+// date format
+console.log(typeof myDate);
+let myCreatedDate = new Date (2025, 07, 19)
+myCreatedDate = new Date (2025, 07, 19, 5, 7)
+myCreatedDate = new Date ("2025-08-15")
+console.log(myCreatedDate.toDateString());
+
+//  time stamp
+
+const myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+
+console.log(Date.now()/1000);
+console.log(Math.floor(Date.now()/1000));
+console.log(Math.round(Date.now()/1000));
+

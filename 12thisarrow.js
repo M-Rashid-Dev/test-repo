6c45b6d9f0481a18e8407ee6947fd7b289f4836a
Rashid <rashidmustafa3355@gmail.com>
2025-09-05 16:08:24 +0500

    this and arrow fnction

---
+++ 12thisarrow.js
@@ -0,0 +1,33 @@
+const user = {
+    username: "rashid",
+    price: 1000,
+    welcomemessage: function(){
+        console.log(`${this.username}, welcome to website`);
+        console.log(this);
+        
+    }
+}
+// console.log(user);
+// user.welcomemessage()
+// user.username= "Muhammad"
+// user.welcomemessage()
+// console.log(this);
+
+//  arrow function
+
+//  explicit return
+
+// const addtwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// implicit return
+
+// const addtwo = (num1, num2) => num1 + num2
+// const addtwo = (num1, num2) => (num1 + num2)
+
+// object in implicit return
+
+const addtwo = (num1, num2) => ({ username: "MUHAMMAD RASHID"})
+
+console.log(addtwo(5, 10));

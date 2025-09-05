b305629a9901fcabdb481cf4c95dc02239d7c14d
Rashid <rashidmustafa3355@gmail.com>
2025-09-05 15:34:32 +0500

    scope level and hoisting

--- 11scope.js
+++ 11scope.js
@@ -8,13 +8,38 @@ if (true){
     let a =5
     const b = 3
     var c = 10
-    console.log(`inner ${a}`);
+    // console.log(`inner ${a}`);
 }
 // console.log(a); here we are accessing a variable out of the scope 
 // console.log(c);
 
-console.log(`outer ${a}`);
+// console.log(`outer ${a}`);
 
-// 
+//  scope level and mini hoisting
 
+function one(){
+    const name = "Rashid"
+    function two(){
+        const age = 28
+        // console.log(name);
+    }
+    // console.log(age);
+    
+    two()
+}
+one()
 
+// hoisting
+
+console.log(addone(10));
+
+function addone(num){
+    return num +1
+}
+
+// add(10)      here we are holding function in variable thats why we can not access
+const add = function(num){
+    return num +2
+}
+
+add(10)

8b20eb07b163734875fc65a46418c1b5240a1412
Rashid <rashidmustafa3355@gmail.com>
2025-09-08 03:55:20 +0500

    do whikle loop

---
+++ 17while.js
@@ -0,0 +1,23 @@
+//  while loopa
+
+let tr = 0
+while (tr <= 10) {
+    // console.log("yes true");
+    tr= tr+2
+}
+
+const fruits = ["apple", "banana", "Orange", "grapes"]
+
+let i = 0
+while (i < fruits.length) {
+    // console.log(` at ${i} index fruit is ${fruits[i]}`);
+    i++
+}
+
+//  do while loop
+let score = 1
+
+do {
+    console.log(` score is ${score}`);
+    score++
+} while (score <= 10);

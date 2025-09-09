033131518e090f23047bb3ff17073000d10aca11
Rashid <rashidmustafa3355@gmail.com>
2025-09-09 16:01:09 +0500

    for each loop

--- 20foreach.js
+++ 20foreach.js
@@ -1,4 +1,50 @@
 const fruits = ["apple", "banana", "Orange", "grapes"]
-fruits.forEach( (name) => {
-    console.log(name);
+
+//  call back function in foreach loop
+fruits.forEach( function (name) {
+    // console.log(name);
+})
+
+
+//  arrow function in for each loop
+fruits.forEach( (item) => {
+    // console.log(item);
+    
+})
+
+function fname(item){
+    // console.log(item);
+    
+}
+//  function  referance in for each loop
+fruits.forEach(fname)
+
+//  other parameters in for each loop ( items, index, arr)
+fruits.forEach((item, index, arr) => {
+    // console.log(item, index, arr);
+    
+})
+
+const narray =[
+    {
+        firstname: "muhmmad",
+        lastname: "Rashid"
+    },
+    {
+        firstname: "muhmmad",
+        lastname: "Tahir"
+    },
+    {
+        firstname: "muhmmad",
+        lastname: "Ali"
+    },
+    {
+        firstname: "muhmmad",
+        lastname: "Hassan"
+    }
+]
+
+narray.forEach((item, index, arr) => {
+    console.log(item, index, arr);
+    
 })
